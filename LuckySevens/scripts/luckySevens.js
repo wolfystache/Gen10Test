a7c9da0be9c4f690f938b125e4c3915a22019977
Arthur Clark <artclark43@gmail.com>
2018-12-20 18:56:03 -0600

    First commit for the Gen10 Test. Should be final commit barring any changes I realize need to be made

---
+++ LuckySevens/scripts/luckySevens.js
@@ -0,0 +1,81 @@
+﻿/*
+Name: Arthur Clark
+Date: 12 / 15 / 2018
+Filename: luckySeven.js
+Description:
+- The script for the lucky seven problem. It contains two functions: One for playing the game, and the other 
+for generating a random dice roll sum, which the first function calls.*/
+
+
+/*
+Function: play()
+Parameters: None 
+Return value: None 
+Description: 
+This function grabs the starting bet, checks to make sure it's a number, then 
+simulates the game by looping until the user runs out of money. The rules for the game are 
+that a die is rolled at every turn. If the sum of the roll is 7, $4 is added to the balance, and 
+if the sum is not $7, $1 is subtracted from the balance. This continues until the balance is less than 
+or equal to 0, where a table is displayed giving the user info about their game
+*/
+function play() {
+	var bet = document.getElementById("startingBet").value; 
+
+	if (bet == "" || isNaN(bet)) {
+		alert("Please enter a valid number for your starting bet");
+		return;
+	}
+
+	var numRolls = 0; 
+	var hiRoll = 0;
+
+	/*The parseFloat function is used to avoid type problems where the current balance is concatenated every turn 
+	instead of being added/subtracted */
+	var currMoney = parseFloat(bet);
+	var maxMoney = parseFloat(bet);
+
+	while (currMoney > 0) {
+		if (currMoney > maxMoney) {
+			maxMoney = currMoney;
+			hiRoll = numRolls;
+		}
+		var roll = rollDice();
+
+		if (roll === 7) {
+			currMoney += 4;
+			
+		}
+		else {
+			currMoney--;
+			
+		}
+		numRolls++;
+	}
+
+	var results = document.getElementById("results");
+	results.style.display = "block";
+	document.getElementById("bet").innerText = bet;
+	document.getElementById("totalRolls").innerText = numRolls;
+	document.getElementById("maxMoney").innerText = maxMoney;
+	document.getElementById("hiRoll").innerText = hiRoll;
+
+
+
+}
+
+/*
+Function: rollDice()
+Parameters: None 
+Return value: Sum of dice 0 and dice 1 
+Description: 
+This function returns of the sum of two random dice rolls
+*/
+
+function rollDice() {
+	var die0, die1 = 0;
+	die0 = Math.floor(6 * Math.random() + 1); 
+
+	die1 = Math.floor(6 * Math.random() + 1); 
+
+	return die0 + die1;
+}
---
+++ RestaurantSite/scripts/restaurantSite.js
@@ -0,0 +1,79 @@
+﻿/*
+Name: Arthur Clark
+Date: 12/16/2018
+Filename: restaurantSite.js 
+Description:
+- This is the script for validating the The Silver Spoon's contact page. It contains only one function, validate(), 
+which validates that the user has submitted valid contact information, and alerts them if they are missing something.
+ */
+
+
+/*
+ Function: validate
+ Parameters: None 
+ Return Value: bool upon successful/unsuccessfuly validation 
+ Description:
+ - This function simply checks the input that the user filled out in the contact me page to see if it's valid. Valid 
+ means that all fields (besides additional info) are filled with something. E-mail must also follow e-mail form convention, 
+ but this is checked by the browser through the type="email" attribute, not in this function. 
+ */
+
+function validate() {
+
+	/* Errors is used as a string to identify any missing input fields the user may have submitted and is given to 
+	them through an alert() message*/
+	var errors = "";
+
+	var name = document.getElementById("nameField").value;
+
+	if (name == "") {
+		errors += "- Please enter your name\n";
+	}
+
+	var email = document.getElementById("emailField").value;
+	if (email.length == 0) {
+		errors += "- Please enter your e-mail\n";	
+	}
+
+
+	var phone = document.getElementById("phoneField").value;
+	if (phone.length == 0) {
+		errors += "- Please enter your phone number\n";
+	}
+
+
+	var reason = document.getElementById("reasonDD").value; 
+	if (reason.length == 0) {
+		errors += "- Please enter your reason for inquiry";
+		
+	}
+
+
+	var days = document.getElementsByName("bestDay");
+
+	// This loop checks that at least one of the checkboxes is checked
+	for (var i = 0; i < days.length; i++) {
+		if (days[i].checked) {
+			break;
+		}
+		else {
+			if (i == (days.length - 1)) {
+				errors += "- Please give us the best days to contact you";
+			}
+
+		}
+	}
+	/* If there are errors, the user is given an alert() message and the function returns false and the user is allowed to fill in the 
+	remaining fields.
+	*/
+	if (errors != "") {
+		alert("Oops!\n" + errors);
+		return false;
+	}
+	// Upon successful validation, the page refreshes and all fields are set back to blank
+	else {
+		alert("Success! We look forward to working with you!");
+		return true;
+	}
+
+}
